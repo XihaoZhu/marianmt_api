a2fd996213c7783c9e99a47377991e96b882e221
Xihao Zhu <xihao.zhu@outlook.com>
2026-04-08 13:47:39 +0100

    All basic function done

--- app/main.py
+++ app/main.py
@@ -4,15 +4,19 @@ from app.translator import translate
 
 app = FastAPI()
 
+
+
+@app.lifespan("startup")
+def load_model():
+    print("Model loaded")
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI is running!"}
 
-# 定义请求体
 class TranslateRequest(BaseModel):
     text: str
 
-# 翻译接口
 @app.post("/translate")
 def translate_text(request: TranslateRequest):
     result = translate(request.text)
--- app/translator.py
+++ app/translator.py
@@ -1,4 +1,8 @@
 from transformers import MarianMTModel, MarianTokenizer
+import torch
+
+
+MAX_LENGTH = 256
 
 MODEL_NAME = "Helsinki-NLP/opus-mt-en-zh"
 
@@ -6,10 +10,15 @@ tokenizer = MarianTokenizer.from_pretrained(MODEL_NAME)
 model = MarianMTModel.from_pretrained(MODEL_NAME)
 
 def translate(text: str) -> str:
+
+    if len(text) > MAX_LENGTH:
+        return ["Text too long"]
+
     """translate text from English to Chinese"""
 
     encoded = tokenizer(text, return_tensors="pt", padding=True)
 
-    translated = model.generate(**encoded)
+    with torch.no_grad():
+        translated = model.generate(**encoded)
 
     return tokenizer.decode(translated[0], skip_special_tokens=True)
